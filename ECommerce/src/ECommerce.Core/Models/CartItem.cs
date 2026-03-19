389ecf23130eb96b6b01c81c76217d8c3cdb5392
Ejidia Uwabeza <uwabezaejidia94@gmail.com>
2026-03-19 09:13:41 +0300

    Add CartItem model

---
+++ ECommerce/src/ECommerce.Core/Models/CartItem.cs
@@ -0,0 +1,17 @@
+namespace ECommerce.Core.Models;
+
+public class CartItem
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public DateTime AddedAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation properties
+    public User User { get; set; } = null!;
+    public Product Product { get; set; } = null!;
+
+    // Computed properties
+    public decimal SubTotal => Product.DiscountPrice ?? Product.Price * Quantity;
+}
